f5d34ff0a447261af17571f150a3d8f4a5490ee1
TN1905 <tringuyenpopo@gmail.com>
2024-11-01 21:49:18 +0700

    test

--- webstoreonline/src/main/java/com/example/storeonline/WebstoreonlineApplication.java
+++ webstoreonline/src/main/java/com/example/storeonline/WebstoreonlineApplication.java
@@ -1,11 +1,9 @@
-package com.example.storeonline.webstoreonline;
+package com.example.storeonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.example.storeonline.webstoreonline.model")
 public class WebstoreonlineApplication {
 
 	public static void main(String[] args) {
