683ee284e2797134e40affa092bd7a90553820f0
TN1905 <tringuyenpopo@gmail.com>
2024-11-01 21:38:37 +0700

    test

--- webstoreonline/src/main/java/com/example/storeonline/webstoreonline/WebstoreonlineApplication.java
+++ webstoreonline/src/main/java/com/example/storeonline/webstoreonline/WebstoreonlineApplication.java
@@ -2,8 +2,10 @@ package com.example.storeonline.webstoreonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.storeonline.webstoreonline.model")
 public class WebstoreonlineApplication {
 
 	public static void main(String[] args) {
